6fa899867f75f49fb375389339b3047e8f45b684
Pavel Kudlanov <pkudlanov@yahoo.com>
2020-07-31 13:48:15 -0400

    authenticating a user on signup automatically

--- halls/views.py
+++ halls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
+
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 from . import models
 
@@ -14,6 +16,13 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
 
 
 class CreateHall(generic.CreateView):
