9e4982e345034ae3b00214f985f871baf5575ed9
Pavel Kudlanov <pkudlanov@yahoo.com>
2020-07-31 13:40:39 -0400

    CreateHall form being validated with user

--- halls/models.py
+++ halls/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Hall(models.Model):
     title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 class Video(models.Model):
@@ -10,3 +15,6 @@ class Video(models.Model):
     url = models.URLField()
     youtube_id = models.CharField(max_length=255)
     hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
