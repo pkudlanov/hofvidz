d4661d478c231694813c2ea73355386a985ae667
Pavel Kudlanov <pkudlanov@yahoo.com>
2020-08-02 00:55:35 -0400

    making the admin view a bit more user friendly

--- halls/admin.py
+++ halls/admin.py
@@ -4,7 +4,14 @@ from . import models
 
 class VideoAdmin(admin.ModelAdmin):
     readonly_fields = ('id', )
+    list_display = ['title', 'hall']
+    list_filter = ['hall']
 
 
-admin.site.register(models.Hall)
+class HallAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user']
+    list_filter = ['user']
+
+
+admin.site.register(models.Hall, HallAdmin)
 admin.site.register(models.Video, VideoAdmin)
