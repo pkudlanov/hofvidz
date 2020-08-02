424aff08fa5c555175cc25cca820caae787c1bd3
Pavel Kudlanov <pkudlanov@yahoo.com>
2020-08-01 22:18:45 -0400

    fixed signup success url to take you to dashboard

--- halls/views.py
+++ halls/views.py
@@ -85,7 +85,7 @@ class DeleteVideo(generic.DeleteView):
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('dashboard')
     template_name = 'registration/signup.html'
 
     def form_valid(self, form):
